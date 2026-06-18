9ff6c5cc99f858889679583bc84b2507ecbf61f8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-06-18 20:04:51 +0530

    Refactor HelloWorld class methods to prevent duplicate definition of the class

--- components/voltas_ac_climate_ir/hello_world.cpp
+++ components/voltas_ac_climate_ir/hello_world.cpp
@@ -8,32 +8,28 @@ namespace esphome
 
         static const char *TAG = "hello_world.sensor";
 
-        class HelloWorld : public esphome::sensor::Sensor, public esphome::PollingComponent
+        void HelloWorld::setup()
         {
+            ESP_LOGCONFIG(TAG, "Setting up Hello World Sensor...");
+        }
 
-            void setup() override
-            {
-                ESP_LOGCONFIG(TAG, "Setting up Hello World Sensor...");
-            }
+        void HelloWorld::loop()
+        {
+            // This method will be called in every loop iteration.
+        }
 
-            void loop() override
-            {
-                // This method will be called in every loop iteration.
-            }
+        void HelloWorld::update()
+        {
+            // This method will be called every time the sensor is updated.
+            // You can use this method to publish new sensor values.
+            // Publish random number for demonstration purposes
+            this->publish_state(42.0);
+        }
 
-            void update() override
-            {
-                // This method will be called every time the sensor is updated.
-                // You can use this method to publish new sensor values.
-                // Publish random number for demonstration purposes
-                this->publish_state(42.0);
-            }
-
-            void dump_config() override
-            {
-                ESP_LOGCONFIG(TAG, "Hello World Sensor:");
-            }
-        };
+        void HelloWorld::dump_config()
+        {
+            ESP_LOGCONFIG(TAG, "Hello World Sensor:");
+        }
 
     } // namespace hello_world
 } // namespace esphome
