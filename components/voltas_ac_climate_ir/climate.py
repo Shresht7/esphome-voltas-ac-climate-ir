72c7e5fa7d47d6829056eb920465ef1827fc71d2
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-08-21 15:26:31 +0530

    Refactor `CONFIG_SCHEMA` definition in `climate.py` to remove unnecessary extension

--- components/voltas_ac_climate_ir/climate.py
+++ components/voltas_ac_climate_ir/climate.py
@@ -11,10 +11,8 @@ AUTO_LOAD = ['climate_ir']
 voltas_ac_climate_ir_ns = cg.esphome_ns.namespace('voltas_ac_climate_ir')
 VoltasACClimateIR = voltas_ac_climate_ir_ns.class_('VoltasACClimateIR', climate_ir.ClimateIR)
 
-# Inherit the standard climate_ir configuration schema and extend it with our custom options
-CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(VoltasACClimateIR).extend({
-    cv.GenerateID(): cv.declare_id(VoltasACClimateIR),
-})
+# Inherit the standard climate_ir configuration schema 
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(VoltasACClimateIR)
 
 async def to_code(config):
-    var = await climate_ir.new_climate_ir(config)
+    await climate_ir.new_climate_ir(config)
